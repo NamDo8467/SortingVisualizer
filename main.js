c8f1f19fdd3d90301f4e79ad524b8decc7b6435e
NamDo8467 <huynhnamdo@gmail.com>
2022-10-04 15:42:11 -0400

    modified the algorithm

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 const startButton = document.querySelector(".start-btn")
 const randomButton = document.querySelector(".random-list")
-let numbers = document.querySelectorAll("input")
+// let numbers = document.querySelectorAll("input")
 const numberList = document.querySelector(".number-list")
-const navItems = document.querySelectorAll(".main-nav ul li")
-// console.log(navItems);
 
-navItems.forEach(navItem => {
-    navItem.addEventListener("click", () => {
-        navItem.style.color = "black"
-        navItem.style.fontWeight = "500"
-    })
-})
 let isSorting = false
 let numberOfLessThanComparison = 1
+let numberOfSwaps = 0
+let numberOfRounds = 0
 
+let swaps = document.querySelector(".swaps")
+let rounds = document.querySelector(".rounds")
 const GO_UP = 2,
     GO_DOWN = 40,
     TRANSLATE_X = 120
@@ -21,15 +17,37 @@ const GO_UP = 2,
 function delay(millisecond) {
     return new Promise(resolve => setTimeout(resolve, millisecond))
 }
+const checkForValidValues = numbers => {
+    for (const n of numbers) {
+        if (n.value == "" || isNaN(parseInt(n.value))) {
+            console.log(n.value)
+            return false
+        }
+    }
+}
 const sort = async () => {
     isSorting = true
     let numbers = document.querySelectorAll("input")
+
+    if (checkForValidValues(numbers) == false) {
+        alert("Please enter all values")
+        numbers.forEach(number => {
+            number.value = ""
+        })
+        return
+    }
     // let count = 0;
     for (let i = 0; i < numbers.length; i++) {
         for (let j = 0; j < numbers.length - 1; j++) {
             startSorting(numbers[j], numbers[j + 1])
             await delay(3000)
         }
+
+        swaps.textContent = numberOfSwaps
+
+        numberOfRounds += 1
+
+        rounds.textContent = numberOfRounds
         if (numberOfLessThanComparison >= 4) {
             // if number of "less than" comparison is greater or equal to 4, it means that the list is sorted
             break
@@ -55,6 +73,7 @@ const startSorting = (number1, number2) => {
             number1.style.top = `${GO_DOWN}%`
             number1.style.transition = "transform 1s ease, top 1.5s ease"
             numberOfLessThanComparison = -1
+            numberOfSwaps += 1
         } else if (parseInt(number1.value) < parseInt(number2.value) || parseInt(number1.value) == parseInt(number2.value)) {
             number1.style.transform = "translateX(0)"
             number1.style.top = `${GO_DOWN}%`
@@ -83,4 +102,6 @@ randomButton.addEventListener("click", () => {
             number.value = Math.floor(Math.random() * 10)
         })
     }
+    swaps.textContent = 0
+    rounds.textContent = 0
 })
